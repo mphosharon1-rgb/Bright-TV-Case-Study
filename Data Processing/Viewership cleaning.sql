0d173bbfa5cdb9c8b662bc3a2ccbb657f2c3a996
mphosharon1-rgb <mphosharon1@gmail.com>
2026-07-28 09:21:11 +0000

    codes

--- Data Processing/Viewership cleaning.sql
+++ Data Processing/Viewership cleaning.sql
@@ -1,3 +1,33 @@
 -- Databricks notebook source
 SELECT *
 FROM bright_tv.customers.viewership;
+-- We are browsing for the data so that we can see what type of data we have before we can start cleaning.
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
+--  We are having two user_ID, the first step is to check if the userid column is complete and it doesnt have NULLS.
+
+SELECT *
+FROM bright_tv.customers.viewership
+WHERE UserID0 IS NULL
+OR    userid4 IS NULL;                                                           --The columns do not have any NULLS
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Checking if the two userID are not equal to each other.
+
+SELECT *
+FROM bright_tv.customers.viewership
+WHERE UserID0<>userid4;
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Checking for duplicates.
+
+SELECT UserID0, RecordDate2,
+COUNT(*) AS duplicates_Count
+FROM bright_tv.customers.viewership
+GROUP BY UserID0, RecordDate2
+HAVING COUNT(*) >1
+ORDER BY duplicates_Count DESC;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
